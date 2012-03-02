046a7e9ecb2f05aba8994c18fc40583092a4754f
chestozo <chestozo@yandex-team.ru>
2012-03-02 20:57:51 +0400

    Suggest: test for data request retries.

--- tests/suggest.js
+++ tests/suggest.js
@@ -29,6 +29,7 @@ test("Do not request while typing. Only after specified delay.", function() {
     // Fake time.
     var time = sinon.useFakeTimers();
 
+
     /* Scenario */
     $input.val("f").keyup();    // Type first letter.
     time.tick(450);             // Wait less then delay.
@@ -36,6 +37,7 @@ test("Do not request while typing. Only after specified delay.", function() {
     time.tick(550);             // Wait more then delay.
 
     server.respond();
+
 
     /* Checks */
     ok(!!suggest.popup, "suggest ready");
@@ -48,11 +50,42 @@ test("Do not request while typing. Only after specified delay.", function() {
 });
 
 test("Retry request data in case of fail", function() {
+    expect(5);
+
     /* Init */
+    // Fake time.
+    var time = sinon.useFakeTimers();
+
+    // Fake server.
+    var server = sinon.fakeServer.create();
+    server.respondWith([ 503, {}, "" ]);
+
+    // Init suggest.
+    var $input = $('<input type="text" data-nb="suggest" data-nb-source-url="/get-suggest" data-nb-delay="500" />').appendTo(document.body);
+    var suggest = nb.block($input[0]); // init new suggest block
+    $input.trigger(jQuery.Event("focusin")); // on this event suggest will initialized.
+
+    // Check suggest block init was called.
+    ok(!!suggest.popup, "suggest ready");
 
     /* Scenario */
+    $input.val("f").keyup();    // Type first letter.
+    time.tick(550);             // Wait more then delay for request to be started.
+
+    // Before first request.
+    equal(server.requests.length, 1, 'Now request was sent');
+    var req = suggest._requests['f'];
+    equal(req.retries, 2, '2 retries left');
+
+    server.respond(); // Server respond with 503 first time.
 
     /* Checks */
+    equal(server.requests.length, 3, "there must be 3 request, that were sent");
+    equal(Object.keys(suggest._requests).length, 0, "there must be no requests: after allowed number of fails request will be removed");
+
+    /* Restore */
+    server.restore();
+    time.restore();
 });
 
 test("After text changed - make more retries", function() {
