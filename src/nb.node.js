d29d35a4e9fde0d02eafadd4127bda88670de0a3
Sergei Nikitin <nik.pasaran@gmail.com>
2012-03-11 18:44:01 +0400

    Методы data и set/get/delMod вынесены в отдельный модуль nb.node. Fixes #32

---
+++ src/nb.node.js
@@ -0,0 +1,91 @@
+//  nb.node
+//  -------
+
+nb.node = {};
+
+(function() {
+
+//  ---------------------------------------------------------------------------------------------------------------  //
+
+nb.node.data = function(node, key, value) {
+    //  Возвращаем или меняем data-атрибут.
+    if (key) {
+        if (value !== undefined) {
+            node.setAttribute('data-nb-' + key, value);
+        } else {
+            return parseValue( node.getAttribute('data-nb-' + key) );
+        }
+    } else {
+        //  Возвращаем все data-атрибуты.
+        var data = {};
+
+        var attrs = node.attributes;
+        var r;
+        for (var i = 0, l = attrs.length; i < l; i++) {
+            var attr = attrs[i];
+            if (( r = /^data-nb-(.+)/.exec(attr.name) )) {
+                data[ r[1] ] = parseValue(attr.value);
+            }
+        }
+
+        return data;
+    }
+
+    function parseValue(value) {
+        var ch = value.charAt(0);
+        return (ch === '[' || ch === '{') ? eval( '(' + value + ')' ) : value;
+    }
+};
+
+//  ---------------------------------------------------------------------------------------------------------------  //
+
+//  Работа с модификаторами.
+
+//  Получить модификатор.
+nb.node.getMod = function(node, name) {
+    return nb.node.setMod(node, name);
+};
+
+var modCache = {};
+
+//  Установить/получить/удалить модификатор.
+nb.node.setMod = function(node, name, value) {
+    //  Например, name равно popup_to. В bem-терминах это значит, что имя блока popup, а модификатора to.
+    //  Ищем строки вида popup_to_left и popup_to (в этом случае, значение модификатора -- true).
+    var rx = modCache[name] || (( modCache[name] = RegExp('(?:^|\\s+)' + name + '(?:_([\\w-]+))?(?:$|\\s+)') ));
+
+    var className = node.className;
+
+    if (value === undefined) {
+        //  Получаем модификатор.
+
+        var r = rx.exec(className);
+        //  Если !r (т.е. r === null), значит модификатора нет вообще, возвращаем '' (FIXME: или нужно возвращать null?).
+        //  Если r[1] === undefined, это значит модификатор со значением true.
+        return (r) ? ( (r[1] === undefined) ? true : r[1] ) : '';
+
+    } else {
+        //  Удаляем старый модификатор, если он там был.
+        className = className.replace(rx, '').trim();
+
+        //  Тут недостаточно просто if (value) { ... },
+        //  потому что value может быть нулем.
+        if (value !== false && value !== '') {
+            //  Устанавливаем новое значение.
+            //  При этом, если значение true, то просто не добавляем часть после _.
+            className += ' ' + name + ( (value === true) ? '' : '_' + value );
+        }
+        node.className = className;
+
+    }
+};
+
+//  Удалить модификатор.
+nb.node.delMod = function(node, name) {
+    nb.node.setMod(node, name, false);
+};
+
+//  ---------------------------------------------------------------------------------------------------------------  //
+
+})();
+
