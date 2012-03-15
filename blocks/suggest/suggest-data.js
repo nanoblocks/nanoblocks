11f0adaa305bc46d09e4b72f5351ee0725553a42
chestozo <chestozo@yandex-team.ru>
2012-03-15 20:25:54 +0400

    Static array data source for suggest.

--- blocks/suggest/suggest-data.js
+++ blocks/suggest/suggest-data.js
@@ -4,6 +4,7 @@
 
 /**
  * Data source.
+ * Any inheritor as usual needs to implement only the get method.
  */
 var ds = function() {};
 
@@ -11,19 +12,33 @@ ds.prototype.init = function() {
     this.cache = {};
 };
 
-ds.prototype.has = function(key) {};
+ds.prototype.has = function(key) {
+    return (key in this.cache);
+};
 
-ds.prototype.get = function(key, callback) {};
+/**
+ * Request data asyncronously.
+ * @param {Object} req Request params.
+ */
+ds.prototype.get = function(req) {};
 
-ds.prototype.getSync = function(key) {};
+ds.prototype.getSync = function(key) {
+    return this.cache[key];
+};
 
-ds.prototype.prepareData = function(data) { return data; };
+/**
+ * Raw data prepare. For example, to prepare data, that come from server.
+ * @param {Object} data Raw data object.
+ */
+ds.prototype.prepareData = function(data) {
+    return data;
+};
 
 // ----------------------------------------------------------------------------------------------------------------- //
 
 /**
  * Ajax data source.
- * @param {Object} options.
+ * @param {{ url: string, retry_count: ?number=3, max_items: ?number=, timeout: number= }} options.
  */
 var ajaxDS = function(options) {
     this.super_.init();
@@ -45,18 +60,6 @@ ajaxDS.prototype.get = function(req) {
         this._createRequest(req);
     }
     this.requests[req.key].retry();
-};
-
-// ----------------------------------------------------------------------------------------------------------------- //
-
-ajaxDS.prototype.getSync = function(key) {
-    return this.cache[key];
-};
-
-// ----------------------------------------------------------------------------------------------------------------- //
-
-ajaxDS.prototype.has = function(key) {
-    return (key in this.cache);
 };
 
 // ----------------------------------------------------------------------------------------------------------------- //
@@ -114,7 +117,90 @@ ajaxDS.prototype._createRequest = function(req) {
 
 // ----------------------------------------------------------------------------------------------------------------- //
 
+/**
+ * Static array data source.
+ * @param {Array.<Object>} ar An array with data.
+ */
+var arrayDS = function(ar, key_name) {
+    this.super_.init();
+    this.key_name = key_name;
+    this._keys = null; // Cache keys for faster search.
+    this.ar = this.prepareData(ar);
+};
+
+nb.inherit(arrayDS, ds);
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
+arrayDS.prototype.prepareData = function(ar) {
+    ar = ar.slice(0); // Create copy, cause sort() modify initial array.
+// TODO кажется, сортировка, всё-таки, не нужна.
+//    var key = this.key_name;
+//    ar.sort(function(a, b) {
+//        return a[key] > b[key];
+//    });
+    return ar;
+};
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
+/**
+ * @param req
+ */
+arrayDS.prototype.get = function(req) {
+    var found = [];
+    var ar = this.ar;
+    var keys = this._keys;
+    var key_name = this.key_name;
+    var query = req.key.toLowerCase();
+    var item;
+    var key;
+    var i;
+
+    req.onstart();
+
+    // If already in cache.
+    if (query in this.cache) {
+        req.onsuccess(this.cache(query));
+        return;
+    }
+
+    // Search.
+    if (keys) {
+        for (i = 0; i < keys.length; i++) {
+            item = ar[i];
+            key = keys[i];
+            if (match(key, query)) {
+                found.push(item);
+            }
+        }
+    }
+    else {
+        keys = this._keys = [];
+        for (i = 0; i < ar.length; i++) {
+            item = ar[i];
+            key = keys[i] = ('' + item[key_name]).toLowerCase();
+            if (match(key, query)) {
+                found.push(item);
+            }
+        }
+    }
+
+    // Save in cache.
+    this.cache[query] = found;
+
+    req.onsuccess(found);
+    return;
+
+    function match(key, query) {
+        return (key.indexOf(query) >= 0);
+    }
+};
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
 nb.suggest = {};
 nb.suggest.ajaxDS = ajaxDS;
+nb.suggest.arrayDS = arrayDS;
 
 }(jQuery));
