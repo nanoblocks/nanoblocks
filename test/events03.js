9894ab1d766d26070e66c1fb3082fc9df497d5d5
chestozo <chestozo@yandex-team.ru>
2013-08-31 21:33:33 +0400

    Тесты для вложенных блоков

--- test/events03.js
+++ test/events03.js
@@ -35,7 +35,7 @@ describe('nested blocks', function() {
         this.$html.remove();
     });
 
-    it('top level block can listen to events inside a nested block', function() {
+    it('nested block: click event inside', function() {
         this.setHTML(
             '<div data-nb="top">' +
                 '<div data-nb="nested">' +
@@ -44,6 +44,45 @@ describe('nested blocks', function() {
                 '</div>' +
             '</div>'
         ).find('.nested-listen-this').trigger('click');
+        expect(this.events.click).to.be.eql( [ 'nested' ] );
+    });
+
+    it('top level block: listen to clicks inside a nested block', function() {
+        this.setHTML(
+            '<div data-nb="top">' +
+                '<div data-nb="nested">' +
+                    '<div class="nested-listen-this"></div>' +
+                    '<div class="top-listen-this"></div>' +
+                '</div>' +
+            '</div>'
+        ).find('.top-listen-this').trigger('click');
+        expect(this.events.click).to.be.eql( [ 'top' ] );
+    });
+
+    it('both can listen to event inside a nested block', function() {
+        this.setHTML(
+            '<div data-nb="top">' +
+                '<div data-nb="nested">' +
+                    '<div class="nested-listen-this"></div>' +
+                    '<div class="top-listen-this"></div>' +
+                    '<div class="both-listen-this"></div>' +
+                '</div>' +
+            '</div>'
+        ).find('.both-listen-this').trigger('click');
+        expect(this.events.click).to.be.eql( [ 'nested', 'top' ] );
+    });
+
+    it('nested block can prevent top level block from receiving the click', function() {
+        this.returns['click'] = { nested: false };
+        this.setHTML(
+            '<div data-nb="top">' +
+                '<div data-nb="nested">' +
+                    '<div class="nested-listen-this"></div>' +
+                    '<div class="top-listen-this"></div>' +
+                    '<div class="both-listen-this"></div>' +
+                '</div>' +
+            '</div>'
+        ).find('.both-listen-this').trigger('click');
         expect(this.events.click).to.be.eql( [ 'nested' ] );
     });
 });
